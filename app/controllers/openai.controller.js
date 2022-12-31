bacb6c6159f6e23fe61704d55bedaf76e01b201f
Martin Nzuki <iamnzuki@gmail.com>
2022-12-31 16:41:36 +0300

    Fix max output issues

--- app/controllers/openai.controller.js
+++ app/controllers/openai.controller.js
@@ -18,11 +18,11 @@ exports.search = async (req, res) => {
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: q,
-        temperature: 0.9,
+        // temperature: 0.9,
         // max_tokens: 150,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0.6,
+        // top_p: 1,
+        // frequency_penalty: 0,
+        // presence_penalty: 0.6,
         // stop: [" Human:", " AI:"],
     });
     // console.log(completion.data.choices[0].text);
